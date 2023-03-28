6f72652f88112df31d7569734744275004050052
madcow95 <lobasketve@naver.com>
2023-03-28 21:40:03 +0900

    StoryBoard UI에서 코드로 element 생성

--- TossBenefitTab_UIKit/TossBenefitTab/BenefitView/ButtonBenefitViewController.swift
+++ TossBenefitTab_UIKit/TossBenefitTab/BenefitView/ButtonBenefitViewController.swift
@@ -12,15 +12,39 @@ class ButtonBenefitViewController: UIViewController {
     var benefit: Benefit = .today
     var benefitDetail: BenefitDetails = .default
     
+    @IBOutlet weak var vStackView: UIStackView!
     @IBOutlet weak var ctaButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
         
-        ctaButton.layer.masksToBounds = true
-        ctaButton.layer.cornerRadius = 5
+        setupUI()
+        addGuides()
         
         ctaButton.setTitle(benefit.ctaTitle, for: .normal)
+    }
+    
+    private func setupUI() {
+        
+        ctaButton.layer.masksToBounds = true
+        ctaButton.layer.cornerRadius = 5
+    }
+    
+    private func addGuides() {
+        let guideViews: [BenefitGuidView] = benefitDetail.guides.map{ guide in
+            let guideView = BenefitGuidView(frame: .zero)
+            guideView.icon.image = UIImage(systemName: guide.iconName)
+            guideView.title.text = guide.guide
+            
+            return guideView
+        }
+        
+        guideViews.forEach{ view in
+            self.vStackView.addArrangedSubview(view)
+            NSLayoutConstraint.activate([
+                view.heightAnchor.constraint(equalToConstant: 60)
+            ])
+        }
     }
 }
