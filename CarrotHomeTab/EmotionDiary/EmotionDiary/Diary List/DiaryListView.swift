656b399f775c2fd4ab1759e7b77fb3e5897df7b4
madcow95 <lobasketve@naver.com>
2023-04-05 22:24:30 +0900

    Navigation View 생성

--- CarrotHomeTab/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
+++ CarrotHomeTab/EmotionDiary/EmotionDiary/Diary List/DiaryListView.swift
@@ -20,21 +20,65 @@ struct DiaryListView: View {
         GridItem(.flexible())
     ]
     var body: some View {
-        LazyVGrid(columns: layout) {
-            ForEach(vm.keys, id:\.self) { key in
-                Section {
-                    let items = vm.dic[key] ?? []
-                    let orderedItems = items.sorted{ $0.date < $1.date }
-                    
-                    ForEach(orderedItems) { item in
-                        MoodDiaryCell(diary: item)
-                            .frame(height: 50)
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: layout) {
+                        ForEach(vm.keys, id:\.self) { key in
+                            Section {
+                                let items = vm.dic[key] ?? []
+                                let orderedItems = items.sorted{ $0.date < $1.date }
+                                
+                                ForEach(orderedItems) { item in
+                                    MoodDiaryCell(diary: item)
+                                        .frame(height: 50)
+                                }
+                            } header: {
+                                Text(formattedSectionTitle(key))
+                                    .font(.system(size: 30, weight: .black))
+                            }
+                            .frame(height: 60)
+                            .padding()
+                        }
                     }
-                } header: {
-                    Text(key)
+                }
+                HStack {
+                    Button {
+                        print("추가!")
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                    }
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.white)
+                    .background(Color.pink)
+                    .cornerRadius(40)
                 }
             }
+            .navigationTitle("Emotion Diary")
         }
+    }
+}
+
+extension DiaryListView {
+    private func formattedSectionTitle(_ id: String) -> String {
+        let dateComponents = id
+            .components(separatedBy: "-")
+            .compactMap{ Int($0) }
+        guard let year = dateComponents.first, let month = dateComponents.last else {
+            return id
+        }
+        
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let dateComponent = DateComponents(calendar: calendar, year: year, month: month)
+        let date = dateComponent.date!
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM yyyy"
+        return formatter.string(from: date)
     }
 }
 
